795c3f81bea1963814c8ece5d349e7f0ddd789cf
Bayu Sedana <bayusedana26@gmail.com>
2022-08-13 11:41:06 +0700

    go modules tutor

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/bayusedana26/app-golang-bayu.git
+
+go 1.19
+
+require github.com/bayusedana26/golang-bayu/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	golang_bayu "github.com/bayusedana26/golang-bayu/v2"
+)
+
+func main() {
+	fmt.Println(golang_bayu.SayHello("Bayu Sedana"))
+}
